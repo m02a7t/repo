6f0ccc4e56dfd3458e71bb4a880697a9e38da3a9
m02a7t <dishmayhem@gmail.com>
2016-08-17 02:30:59 -0400

    Add files via upload
    v1.1

--- circuitobject.cs
+++ circuitobject.cs
@@ -6,16 +6,66 @@ using System.Threading.Tasks;
 
 namespace circuitobject
 {
-    public class node 
+    public class component : node
     {
-        public nodetype type;
+        public string type;
+        public string value;
+        public int pins;
+        public int[] pinsarray;
+        public string[] nets;
+        public bool iswithinlimits;
+         
+        public component()
+        {
+            string name = "NO NAME";
+        }
+
+        public component(string name) // constructor requires component name from textexec
+        {
+            this.name = name;
+            this.type = dataTable.Row[name].rows["type"];
+            this.value = dataTable.Row[name].rows["value"];
+            this.neighborsCount = nieghbors.Count();
+
+            this.iswithinlimits = checklimits(value);
 
 
-        // node constructor gets neighbors
-        
+
+        }
+                //to do get pins, pins array from parser (revise parser)
+            public bool checklimits(string value)
+        {
+            bool withinlims = true;
+
+            //to do limit checker. separate value to float val and string units
+
+            return withinlims;
+        }
+
+    } // end component class
+
+    public class node
+    {
+        public string name { get; set; }
+        public int neighborsCount;
+        public List<node> nieghbors;
+
+        public node()
+        {
+            name = "NO NAME";
+        }
+
+        public node(string name)
+        {
+          
+            
+
+
+        }
+    } // end class node
+} // end namespace circuitobject
 
 
 
 
-    }
-}
+
