6e8320828a7c284e6d1cf9d1c3980ce298898a5b
m02a7t <dishmayhem@gmail.com>
2016-08-17 00:40:50 -0400

    Add files via upload

---
+++ circuitobject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circuitobject
+{
+    public class node 
+    {
+        public nodetype type;
+
+
+        // node constructor gets neighbors
+        
+
+
+
+
+    }
+}
