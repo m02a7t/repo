eb5346f3be89d4d61a6a87327611447678fd08f2
m02a7t <dishmayhem@gmail.com>
2016-08-17 02:32:36 -0400

    Add files via upload
    v1

---
+++ testexecutive.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circuitobject
+{
+    class testexecutive
+    {
+        static void main(string[] argc)
+        {
+
+            string[] components = { "C90", "C180", "R270" };
+
+            foreach (string compName in components)
+            {
+                component c = new component(compName);
+                if (c.hasNeighbors)
+                {
+
+
+
+                }
+
+            }
+
+
+
+        }
+
+
+    }
+}
